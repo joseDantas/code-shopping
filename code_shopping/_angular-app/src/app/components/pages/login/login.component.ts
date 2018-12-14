e348020417b24fc08de4ddcb1063faa9e8ea87e7
joseDantas <jodantas20@gmail.com>
2018-12-14 10:33:54 -0300

    Revert "adfgser"
    This reverts commit fdf4cb240d70c6c5efa16d4c5ed0a53841f43eb7.

--- code_shopping/_angular-app/src/app/components/pages/login/login.component.ts
+++ code_shopping/_angular-app/src/app/components/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import {HttpClient} from '@angular/common/http';
+import {subscribeOn} from "rxjs/operators";
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  credentials = {
+    email: '',
+      password: ''
+  }
+  constructor(private http: HttpClient) {   //injeção de dependencia automatica
+
+  }
 
   ngOnInit() {
   }
 
+  submit(){
+      this.http.post<any>('http://localhost:8000/api/login', this.credentials)
+          .subscribe((data) =>{
+            const token = data.token;
+            this.http.get('http://localhost:8000/api/categories', {
+              headers: {
+                'Authorization': `Bearer ${token}`
+              }
+            })
+                .subscribe(data => console.log(data));
+          });
+      return false
+  }
+
 }
+
+//comando <any> faz com que o JS aceite qualquer tipo de dados

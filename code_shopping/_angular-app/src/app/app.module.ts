c83edab5f72ab16e26d541ac4bfdef430791d91e
joseDantas <jodantas20@gmail.com>
2018-12-14 11:42:28 -0300

    Definindo pagina padrao

--- code_shopping/_angular-app/src/app/app.module.ts
+++ code_shopping/_angular-app/src/app/app.module.ts
@@ -3,18 +3,45 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/pages/login/login.component';
+<<<<<<< HEAD
 import {FormsModule} from "@angular/forms";
+=======
+import { CategoryListComponent } from './components/pages/category/category-list/category-list.component';
+import {FormsModule} from "@angular/forms";
+import {RouterModule, Routes} from "@angular/router";
+>>>>>>> angular
 import {HttpClientModule} from '@angular/common/http';
 
+
+
+const routes: Routes = [    //mapeamento de rotas
+     {
+        path: 'login', component: LoginComponent
+     },
+    {
+        path: 'categories/list', component: CategoryListComponent
+    },
+    {       //quando a página estiver totalmente vazia, redireciona para a página de login
+        path: '',
+        redirectTo: '\login',
+        pathMatch: 'full'
+    }
+    ];
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    CategoryListComponent
   ],
   imports: [
       BrowserModule,
       FormsModule,
+<<<<<<< HEAD
       HttpClientModule
+=======
+      HttpClientModule,
+      RouterModule.forRoot(routes)
+>>>>>>> angular
   ],
   providers: [],
   bootstrap: [AppComponent]
--- code_shopping/_angular-app/src/app/components/pages/login/login.component.ts
+++ code_shopping/_angular-app/src/app/components/pages/login/login.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+<<<<<<< HEAD
 import {subscribeOn} from "rxjs/operators";
+=======
+import {Router} from "@angular/router";
+
+>>>>>>> angular
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -12,7 +17,11 @@ export class LoginComponent implements OnInit {
     email: '',
       password: ''
   }
+<<<<<<< HEAD
   constructor(private http: HttpClient) {   //injeção de dependencia automatica
+=======
+  constructor(private http: HttpClient, private router: Router) {   //injeção de dependencia automatica
+>>>>>>> angular
 
   }
 
@@ -22,6 +31,7 @@ export class LoginComponent implements OnInit {
   submit(){
       this.http.post<any>('http://localhost:8000/api/login', this.credentials)
           .subscribe((data) =>{
+<<<<<<< HEAD
             const token = data.token;
             this.http.get('http://localhost:8000/api/categories', {
               headers: {
@@ -29,7 +39,17 @@ export class LoginComponent implements OnInit {
               }
             })
                 .subscribe(data => console.log(data));
+=======
+              this.router.navigate(['categories/list']);
+            const token = data.token;
+            //this.http.get('http://localhost:8000/api/categories', {
+             // headers: {
+              //  'Authorization': `Bearer ${token}`
+              //}
+            //})
+               // .subscribe(data => console.log(data));
 
+>>>>>>> angular
           });
       return false
   }

d0d76921b40633f5c3bf87bf6f5d9fecb513f3fb
joseDantas <jodantas20@gmail.com>
2018-12-17 09:59:20 -0300

    criando evento personalizados em um componente

--- code_shopping/_angular-app/src/app/components/bootstrap/alert-error/alert-error.component.ts
+++ code_shopping/_angular-app/src/app/components/bootstrap/alert-error/alert-error.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'alert-error',
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./alert-error.component.css']
 })
 export class AlertErrorComponent implements OnInit {
+  //mensagem de erro de login
+  @Output()
+      showChange: EventEmitter<boolean> = new EventEmitter<boolean>();
+      _show = false;
 
   constructor() { }
 
   ngOnInit() {
   }
+    //mensagem de erro de login
+  @Input()
+  set show(value){
+    this._show = value;
+    this.showChange.emit(value);
+  }
+//esconde mensagem de erro de login
+  hide(){
+    this.show = false;
+  }
 
 }
+//modo correto de chamar evento OUTPUT [(nome do evento)]
